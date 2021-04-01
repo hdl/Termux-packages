7a191c542ed155f5add7691de15f75555b402c6e
umarcor <unai.martinezcorral@ehu.eus>
2021-04-01 08:45:08 +0200

    test NULL context

--- scripts/prog/usbinit.c
+++ scripts/prog/usbinit.c
@@ -3,7 +3,16 @@
 #include <libusb-1.0/libusb.h>
 
 int main(int argc, char **argv) {
-  libusb_context *context;
-  assert(!libusb_init(&context));
+  const struct libusb_version* version = libusb_get_version();
+  printf("Using libusb v%d.%d.%d.%d\n\n", version->major, version->minor, version->micro, version->nano);
+
+  libusb_set_option(NULL, LIBUSB_OPTION_WEAK_AUTHORITY); // MUST be called before init
+
+  libusb_context *context = NULL;
+  int r;
+  r = libusb_init(&context);
+  printf("libusb_init: %d", r);
+  assert(!r);
+
   libusb_exit(context);
 }
